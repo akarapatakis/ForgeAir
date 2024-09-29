eafa9a94b11554acedc4e261f4db794260a15996
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2024-09-29 13:01:36 +0300

    ForgeAir.Core: Added SDK Call Handlers (ConfigurationManager)

---
+++ ForgeAir.Core/SDK/Interfaces/IPluginConfigurationManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForgePlugin;
+using ForgePlugin.Helpers;
+
+namespace ForgeAir.Core.SDK.Interfaces
+{
+    public class IPluginConfigurationManager : ForgePlugin.Helpers.IConfigurationManager
+    {
+
+        IPluginEntry? pluginEntry;
+
+        PluginConfigurationManager configurationManager = new PluginConfigurationManager();
+
+
+        string IConfigurationManager.RetreiveSetting(string key, IPluginEntry plugin)
+        {
+            return configurationManager.RetreiveString(plugin.name, key, plugin.pluginAssemblyName).ToString();
+        }
+
+        void IConfigurationManager.SetSetting(string key, string value, IPluginEntry plugin)
+        {
+            configurationManager.SetString(plugin.name, key, value, plugin.pluginAssemblyName);
+        }
+
+    }
+}
---
+++ ForgeAir.Core/SDK/PluginConfigurationManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+using ForgePlugin;
+
+namespace ForgeAir.Core.SDK
+{
+    public class PluginConfigurationManager
+    {
+
+        readonly bool Initialized = true;
+        string? configFile;
+        IniData? data;
+
+        public string RetreiveString(string category, string key, string iniSafeFileName)
+        {
+            configFile = String.Format("{0}.ini", iniSafeFileName);
+            if (Initialized)
+            {
+                var parser = new FileIniDataParser();
+                IniData data = parser.ReadFile(configFile);
+                if (data[category][key].ToString() == null)
+                {
+                    return String.Empty;
+                }
+                return data[category][key].ToString();
+            }
+            else
+            {
+                PluginConfigurationManager? cfgmg = new PluginConfigurationManager();
+                cfgmg.RetreiveString(category, key, iniSafeFileName);
+                cfgmg = null;
+                return String.Empty;
+            }
+        }
+        public void SetString(string category, string key, string value, string iniSafeFileName)
+        {
+            configFile = String.Format("{0}.ini", iniSafeFileName);
+            if (Initialized)
+            {
+                var parser = new FileIniDataParser();
+                IniData data = parser.ReadFile(configFile);
+                data[category][key] = value;
+                parser.WriteFile(configFile, data);
+            }
+            else
+            {
+                PluginConfigurationManager? cfgmg = new PluginConfigurationManager();
+                cfgmg.SetString(category, key, value, iniSafeFileName);
+                cfgmg = null;
+            }
+        }
+    }
+}
