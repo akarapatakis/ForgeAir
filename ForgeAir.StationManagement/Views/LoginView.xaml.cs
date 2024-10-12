29e46819c4446b80047a10176f96a23e3e0a98e1
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2024-10-12 23:28:22 +0300

    ForgeAir.StationManagement: Implemented Caliburn.Micro & Added Login View

--- ForgeAir.StationManagement/Views/LoginView.xaml.cs
+++ ForgeAir.StationManagement/Views/LoginView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ForgeAir.StationManagement
+namespace ForgeAir.StationManagement.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
