fe998b987c3f393a92c95b7a7391150f4e61769d
Andrei Vas <114833527+andreiv1@users.noreply.github.com>
2024-10-14 00:13:48 +0300

    Added Tracks service

---
+++ ForgeAir.Core/Services/Interfaces/ITracksService.cs
@@ -0,0 +1,16 @@
+﻿using ForgeAir.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgeAir.Core.Services.Interfaces
+{
+    public interface ITracksService
+    {
+        Task<IEnumerable<Track>> GetTracks(int skip, int take);
+        Task<int> GetTrackCountAsync(string query = "");
+        Task<Track> GetTrack(int id);
+    }
+}
---
+++ ForgeAir.Core/Services/TracksService.cs
@@ -0,0 +1,37 @@
+﻿using ForgeAir.Core.Services.Interfaces;
+using ForgeAir.Core.Services.Models;
+using ForgeAir.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgeAir.Core.Services
+{
+    public class TracksService : ITracksService
+    {
+        private readonly IDbContextFactory<ForgeAirDbContext> dbContextFactory;
+
+        public TracksService(IDbContextFactory<ForgeAirDbContext> dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+        public Task<Track> GetTrack(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetTrackCountAsync(string query = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Track>> GetTracks(int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
