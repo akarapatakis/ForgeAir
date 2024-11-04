676f2fa7ef8ff20323a6f2259623da986ecacf0e
ChocolateAdventurouz <aggeloskarapatakis@hotmail.com>
2024-11-04 17:33:08 +0200

    ForgeAir.Core: Added boilerplate OutputDevice instance

---
+++ ForgeAir.Core/Shared/OutputDevice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgeAir.Core.Shared
+{
+    public class OutputDevice
+    {
+        private static Dictionary<string, OutputDevice> instances = new Dictionary<string, OutputDevice>();
+        private Dictionary<string, object> data = new Dictionary<string, object>();
+
+        public OutputDevice() { return; }
+
+        public static OutputDevice RetreiveOrCreate(string name)
+        {
+            if (!instances.ContainsKey(name))
+            {
+                instances[name] = new OutputDevice();
+            }
+            return instances[name];
+        }
+    }
+}
