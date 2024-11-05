2545b96a04689275ccdd237a6266f81d7de5a052
ChocolateAdventurouz <aggeloskarapatakis@hotmail.com>
2024-11-05 18:12:30 +0200

    ForgeAir.Core: Added explainations to OutputDevice's arguments

--- ForgeAir.Core/Shared/OutputDevice.cs
+++ ForgeAir.Core/Shared/OutputDevice.cs
@@ -7,6 +7,9 @@ using ForgeAir.Core.AudioEngine.Enums;
 
 namespace ForgeAir.Core.Shared
 {
+    /// <summary>
+    /// Declares an Output Device so it can be easily initialized with BASS for an 'infinite' amount of Output devices for any use case
+    /// </summary>
     public class OutputDevice
     {
         private static Dictionary<string, OutputDevice> instances = new Dictionary<string, OutputDevice>();
@@ -14,15 +17,39 @@ namespace ForgeAir.Core.Shared
 
         public OutputDevice() { return; }
 
+        /// <summary>
+        /// MME, WASAPI, DirectSound, ASIO
+        /// </summary>
         public DeviceOutputMethodEnum deviceOutputMethod { get; set; }
 
-        public int deviceIndex { get; set; }
+        /// <summary>
+        /// deviceIndex can be obtained through GetDevices(DeviceOutputMethodEnum driver) method in the Devices class, with 'driver' argument, the driver BASS is going to be initialized.
+        /// </summary>
         public int deviceIndex { get; set; } = 0;
+
+        /// <summary>
+        /// Can only be used in WaveOut/MME device initialization
+        /// </summary>
         public bool useDSound {  get; set; }
+
+        /// <summary>
+        /// if no sampleRate is assigned, 44100hz is going to be the fallback option
+        /// </summary>
         public int sampleRate { get; set; } = 44100;
+
+        /// <summary>
+        /// if no audioChannels are assigned, 2 (Stereo) are going to be the fallback option (default in most setups)
+        /// </summary>
         public DeviceOutputAudioChannelsEnum audioChannels { get; set; } = DeviceOutputAudioChannelsEnum.Stereo;
 
+        /// <summary>
+        /// if no bitDepth is assigned, 16-bit is going to be the fallback option (default in most setups)
+        /// </summary>
         public DeviceOutputBitDepthEnum bitDepth { get; set; } = DeviceOutputBitDepthEnum.SixteenBit;
+        
+        /// <summary>
+        /// Should be CPU-dependent. While most recent CPUs/Sound Cards can achieve as low as 50ms, there should be anyway a customizable bufferLength for special cases & older machines
+        /// </summary>
         public int bufferLength { get; set; } = 150;
 
         public static OutputDevice RetreiveOrCreate(string name)
