d537f3cdbd435813797400a9a67f0375956368cf
ChocolateAdventurouz <aggeloskarapatakis@hotmail.com>
2024-11-05 17:53:05 +0200

    ForgeAir.Core: Use audioengine pre-defined enums inside OutputDevice instead of normal datatypes

--- ForgeAir.Core/Shared/OutputDevice.cs
+++ ForgeAir.Core/Shared/OutputDevice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ForgeAir.Core.AudioEngine.Enums;
 
 namespace ForgeAir.Core.Shared
 {
@@ -12,6 +13,16 @@ namespace ForgeAir.Core.Shared
         private Dictionary<string, object> data = new Dictionary<string, object>();
 
         public OutputDevice() { return; }
+
+        public DeviceOutputMethodEnum deviceOutputMethod { get; set; }
+
+        public int deviceIndex { get; set; }
+        public bool useDSound {  get; set; }
+        public int sampleRate { get; set; } = 44100;
+        public DeviceOutputAudioChannelsEnum audioChannels { get; set; } = DeviceOutputAudioChannelsEnum.Stereo;
+
+        public DeviceOutputBitDepthEnum bitDepth { get; set; } = DeviceOutputBitDepthEnum.SixteenBit;
+        public int bufferLength { get; set; } = 150;
 
         public static OutputDevice RetreiveOrCreate(string name)
         {
