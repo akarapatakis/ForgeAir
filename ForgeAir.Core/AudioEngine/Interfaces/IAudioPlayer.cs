6f247c566cd71dcafbfe4569e03f26b686b58083
ChocolateAdventurouz <aggeloskarapatakis@hotmail.com>
2024-11-05 18:16:35 +0200

    ForgeAir.Core: Added Interface to AudioPlayer in order to keep things simple and as broken-down as possible

---
+++ ForgeAir.Core/AudioEngine/Interfaces/IAudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgeAir.Core.AudioEngine.Interfaces
+{
+    public interface IAudioPlayer
+    {
+        void Play();
+        void PlayNext();
+        void Stop();
+        void FadeStop();
+        void Pause();
+        void FadeNext();
+        void Next();
+    }
+}
