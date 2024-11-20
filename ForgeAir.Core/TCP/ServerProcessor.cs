a10965142947d4d712fb5c91565a64c2e3ded681
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2024-11-20 11:04:58 +0200

    ForgeAir.Core: TCP Dummy Files

---
+++ ForgeAir.Core/TCP/ServerProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgeAir.Core.TCP
+{
+    public class ServerProcessor
+    {
+        private TcpListener _server;
+        private readonly string _ipAddress;
+        private readonly int _port;
+    }
+}
