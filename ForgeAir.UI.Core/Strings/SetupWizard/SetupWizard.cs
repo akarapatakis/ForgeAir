fbcdc2bd5b97cd44a2e46f8cf18e2d31951802cf
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2024-11-22 21:56:29 +0200

    ForgeAir.UI.Core: Uploaded files & added initial langagues

---
+++ ForgeAir.UI.Core/Strings/SetupWizard/SetupWizard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ForgeAir.UI.Core.Strings.SetupWizard
+{
+    public class SetupWizard
+    {
+        public ResourceDictionary returnDict(string lang)
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+            dict.Source = new Uri("/Strings/SetupWizard/" + lang + ".xaml", UriKind.Relative);
+            return dict;
+        }
+    }
+}
