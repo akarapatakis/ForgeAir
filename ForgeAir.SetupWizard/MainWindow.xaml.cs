5ab66c9b9631431d97d509b633099d36131be4a5
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2024-11-26 23:11:05 +0200

    ForgeAir.SetupWizard: Dummy load

--- ForgeAir.SetupWizard/MainWindow.xaml.cs
+++ ForgeAir.SetupWizard/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using HandyControl.Controls;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace ForgeAir.SetupWizard
         public MainWindow()
         {
             InitializeComponent();
+            pageViewer.Content = new Pages.DummyLoadingPage();
         }
     }
 }
