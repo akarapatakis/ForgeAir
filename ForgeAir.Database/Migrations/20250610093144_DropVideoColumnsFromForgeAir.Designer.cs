8603a8af63b03278eafa5efcfb8b5fe4e3e111ec
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-06-10 12:38:31 +0300

    .

--- ForgeAir.Database/Migrations/20250610093144_DropVideoColumnsFromForgeAir.Designer.cs
+++ ForgeAir.Database/Migrations/20250610093144_DropVideoColumnsFromForgeAir.Designer.cs
@@ -4,6 +4,7 @@ using ForgeAir.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ForgeAir.Database.Migrations
 {
     [DbContext(typeof(ForgeAirDbContext))]
-    partial class ForgeAirDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250610093144_DropVideoColumnsFromForgeAir")]
+    partial class DropVideoColumnsFromForgeAir
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
