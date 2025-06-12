62d34405dfe9859a3b5a55d27343ccc5f2dbaafe
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-06-13 02:35:26 +0300

    Remove legacy configuration manager package and replace with a custom one

--- ForgeAir.Core/SDK/PluginConfigurationManager.cs
+++ ForgeAir.Core/SDK/PluginConfigurationManager.cs
@@ -3,56 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IniParser;
-using IniParser.Model;
 using ForgeAirPlugin;
 
 namespace ForgeAir.Core.SDK
 {
     public class PluginConfigurationManager
     {
-
-        readonly bool Initialized = true;
+        Helpers.ConfigurationManager configurationManager;
         string? configFile;
-        IniData? data;
 
         public string RetreiveString(string category, string key, string iniSafeFileName)
         {
             configFile = String.Format("{0}.ini", iniSafeFileName);
-            if (Initialized)
-            {
-                var parser = new FileIniDataParser();
-                IniData data = parser.ReadFile(configFile);
-                if (data[category][key].ToString() == null)
-                {
-                    return String.Empty;
-                }
-                return data[category][key].ToString();
-            }
-            else
-            {
-                PluginConfigurationManager? cfgmg = new PluginConfigurationManager();
-                cfgmg.RetreiveString(category, key, iniSafeFileName);
-                cfgmg = null;
-                return String.Empty;
-            }
+            configurationManager = new Helpers.ConfigurationManager(configFile);
+
+            return configurationManager.Get(category, key);
         }
         public void SetString(string category, string key, string value, string iniSafeFileName)
         {
             configFile = String.Format("{0}.ini", iniSafeFileName);
-            if (Initialized)
-            {
-                var parser = new FileIniDataParser();
-                IniData data = parser.ReadFile(configFile);
-                data[category][key] = value;
-                parser.WriteFile(configFile, data);
-            }
-            else
-            {
-                PluginConfigurationManager? cfgmg = new PluginConfigurationManager();
-                cfgmg.SetString(category, key, value, iniSafeFileName);
-                cfgmg = null;
-            }
+            configurationManager = new Helpers.ConfigurationManager(configFile);
+
+            configurationManager.Set(category, key, value);
+            return;
         }
     }
 }
