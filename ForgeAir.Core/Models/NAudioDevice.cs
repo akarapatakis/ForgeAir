29583face84ccd0e25cf38e47793df781753b560
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-06-14 17:33:33 +0300

    NAudioDevice population

---
+++ ForgeAir.Core/Models/NAudioDevice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace ForgeAir.Core.Models
+{
+    public class NAudioDevice
+    {
+
+        public required OutputDevice TargetDevice { get; set; }
+        public WaveOutEvent? WaveOutAPI { get; set; }
+
+        public DirectSoundOut? DSoundAPI { get; set; }
+
+        public WasapiOut? WasapiOutAPI { get; set; }
+
+        public AsioOut? AsioOutAPI { get; set; }
+
+        public object? GetAPI() // non-static here because it needs to be used with each declared model specifically
+        {
+            switch (TargetDevice.API)
+            {
+                case AudioEngine.Enums.DeviceOutputMethodEnum.MME:
+                    return WaveOutAPI;
+                case AudioEngine.Enums.DeviceOutputMethodEnum.DirectSound:
+                    return DSoundAPI;
+                case AudioEngine.Enums.DeviceOutputMethodEnum.WASAPI:
+                    return WasapiOutAPI;
+                case AudioEngine.Enums.DeviceOutputMethodEnum.ASIO:
+                    return AsioOutAPI;
+                default: return null;
+            }
+        }
+    }
+    
+}
