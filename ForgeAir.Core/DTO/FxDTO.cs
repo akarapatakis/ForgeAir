c509f0e75b5080f0e46032d92c66b92c85bc10e9
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-06-25 02:56:57 +0300

    Add FxDTO

---
+++ ForgeAir.Core/DTO/FxDTO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForgeAir.Database.Models;
+using ForgeAir.Database.Models.Enums;
+
+namespace ForgeAir.Core.DTO
+{
+    public class FxDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FilePath { get; set; }
+        public string Color { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateModified { get; set; }
+        public DateTime? DateDeleted { get; set; }
+        public TimeSpan Duration { get; set; }
+        public TrackStatus Status { get; set; }
+
+        public static FxDTO FromEntity(FX fx)
+        {
+            return new FxDTO
+            {
+                Id = fx.Id,
+                Name = fx.Name,
+                FilePath = fx.FilePath,
+                Color = fx.Color,
+                DateAdded = fx.DateAdded,
+                DateModified = fx.DateModified,
+                DateDeleted = fx.DateDeleted,
+                Duration = fx.Duration,
+                Status = fx.Status,
+            };
+        }
+
+        public static FX ToEntity(FxDTO fx)
+        {
+            return new FX
+            {
+                Id = fx.Id,
+                Name = fx.Name,
+                FilePath = fx.FilePath,
+                Color = fx.Color,
+                DateAdded = fx.DateAdded,
+                DateModified = fx.DateModified,
+                DateDeleted = fx.DateDeleted,
+                Duration = fx.Duration,
+                Status = fx.Status,
+            };
+        }
+    }
+}
