31c1191671ce15cce17f703028b0d61e8d2215fb
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-06-25 03:10:04 +0300

    add win32 project

---
+++ ForgeAir.UI.Win32/Controls/DirectoryBrowser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForgeAir.UI.Win32.Controls.Interfaces;
+
+namespace ForgeAir.UI.Win32.Controls
+{
+    public class DirectoryBrowser : IDirectoryBrowser
+    {
+        private FolderBrowserDialog _dialog;
+        public string Title { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Environment.SpecialFolder RootDirectory { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SelectedPath { get => _dialog.SelectedPath ?? String.Empty; }
+        
+        public DirectoryBrowser() {
+            _dialog = new FolderBrowserDialog();
+            _dialog.Description = Title;
+            _dialog.ShowNewFolderButton = false;
+            _dialog.RootFolder = RootDirectory;
+            _dialog.UseDescriptionForTitle = true;
+            _dialog.ShowPinnedPlaces = true;
+            _dialog.AutoUpgradeEnabled = true;
+            
+        }
+
+       
+
+        public bool ShowDialog()
+        {
+            if (_dialog == null)
+            {
+                throw new NullReferenceException();
+            }
+            switch (_dialog.ShowDialog())
+            {
+                case DialogResult.OK:
+                    
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ ForgeAir.UI.Win32/Controls/Interfaces/IDirectoryBrowser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace ForgeAir.UI.Win32.Controls.Interfaces
+{
+    public interface IDirectoryBrowser
+    {
+        string Title { get; set; }
+        Environment.SpecialFolder RootDirectory { get; set; }
+         string SelectedPath { get; }
+        bool ShowDialog();
+    }
+}
