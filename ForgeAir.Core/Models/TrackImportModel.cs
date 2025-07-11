060c933c7b76ef8717546f0021f91edd8df264db
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-07-11 14:55:24 +0300

    add support for categories and utilize importstatus

--- ForgeAir.Core/Models/TrackImportModel.cs
+++ ForgeAir.Core/Models/TrackImportModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +12,67 @@ using ForgeAir.Database.Models.Enums;
 
 namespace ForgeAir.Core.Models
 {
-    public class TrackImportModel
+    public class TrackImportModel : INotifyPropertyChanged
     {
-        public Dictionary<ImportTrackStatusEnum, ImportTrackErrorsEnum> ImportStatus { get; set; }
+        private ImportTrackStatusEnum _importStatus;
+        public ImportTrackStatusEnum ImportStatus
+        {
+            get => _importStatus;
+            set
+            {
+                _importStatus = value;
+                OnPropertyChanged(nameof(ImportStatus));
+            }
+        }
+
+        private ImportTrackErrorsEnum _importErrors;
+        public ImportTrackErrorsEnum ImportErrors
+        {
+            get => _importErrors;
+            set
+            {
+                _importErrors = value;
+                OnPropertyChanged(nameof(ImportErrors));
+            }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
         public string FilePath { get; }
 
         public TrackType TrackType { get; }
         public string? OverrideArtistString { get; } = null;
 
+        private ObservableCollection<CategoryDTO>? _categories;
+        public ObservableCollection<CategoryDTO>? Categories
+        {
+            get => _categories;
+            set
+            {
+                _categories = value;
+                OnPropertyChanged(nameof(Categories));
+            }
+        }
+
         public string? StreamDisplayTitle { get; set; } = null;
         public TimeSpan CrossfadeTime { get; }
-        public TrackImportModel(string filepath, TrackType type, TimeSpan crossfadeTime, bool isVideo = false, string? artistString = null) {
+        public TrackImportModel(string filepath, string? StreamdisplayTitle, TrackType type, TimeSpan crossfadeTime, ObservableCollection<CategoryDTO> categories, bool isVideo = false, string? artistString = null) {
             FilePath = filepath;
             TrackType = type;
+            Categories = categories;
+            if (type == TrackType.Rebroadcast) { StreamDisplayTitle = StreamdisplayTitle; }
             OverrideArtistString = artistString;
             CrossfadeTime = crossfadeTime;
 
--- ForgeAir.Core/Services/Managers/TrackManager.cs
+++ ForgeAir.Core/Services/Managers/TrackManager.cs
@@ -3,29 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ForgeAir.Core.DTO;
 using ForgeAir.Core.Tracks.Enums;
 using ForgeAir.Database;
 using ForgeAir.Database.Models;
+using ForgeAir.Database.Models.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace ForgeAir.Core.Services.Managers
 {
     public class TrackManager
     {
-        private readonly Track track;
+        private readonly TrackDTO track;
 
-        public TrackManager(Track inTrack) { 
-            track = inTrack;
+        public TrackManager(TrackDTO _track) { 
+            track = _track;
         }
         public async Task Delete()
         {
             using (var _context = new ForgeAirDbContext()) {
-                 _context.Tracks.Remove(track);
+                 _context.Tracks.Remove(TrackDTO.ToEntity(track));
                 await _context.SaveChangesAsync();
-
             }
             return;
-
+        }
+        public async Task ChangeStatus()
+        {
+            TrackStatus newStatus = track.TrackStatus == TrackStatus.Enabled ? TrackStatus.Disabled : TrackStatus.Enabled;
+            using (var _context = new ForgeAirDbContext())
+            {
+                track.TrackStatus = newStatus;
+                await _context.SaveChangesAsync();
+            }
+            return;
         }
     }
 }
