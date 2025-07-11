e77e0896abbacc45d3c8febe209777ef112fb9b4
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-07-11 15:09:02 +0300

    add fx import
    this class might get removed or transferred once i make my mind up

--- ForgeAir.Core/Tracks/Importer.cs
+++ ForgeAir.Core/Tracks/Importer.cs
@@ -13,8 +13,10 @@ namespace ForgeAir.Core.Tracks
     public class Importer
     {
         public void AddCategory(CategoryDTO category) {
+            if (category == null) { return; }
             using (var context = new ForgeAirDbContext())
             {
+
                 context.Category.Add(CategoryDTO.ToEntity(category));
                 context.SaveChanges();
                 context.ChangeTracker.Clear();
@@ -22,7 +24,17 @@ namespace ForgeAir.Core.Tracks
             }
         }
 
+        public void AddFx(FxDTO fx) {
+            if (fx == null) { return; }
 
+            using (var context = new ForgeAirDbContext())
+            {
+                context.Fx.Add(FxDTO.ToEntity(fx));
+                context.SaveChanges();
+                context.ChangeTracker.Clear();
+                return;
+            }
+        }
         public static ICollection<TrackDTO> M3UToTracks(string m3uFile) {
             ICollection<TrackDTO> tracks = new List<TrackDTO>();
             var listTracks = m3uParser.M3U.ParseFromFile(m3uFile);
