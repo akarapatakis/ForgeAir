348f887eacf179547bbf9ea00db2a63494ca1f51
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2025-07-11 14:54:18 +0300

    weather api bringup

---
+++ ForgeAir.Core/Services/Weather/IWeatherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenSky.Core.Configuration;
+using OpenSky.Core.Service;
+
+
+namespace ForgeAir.Core.Services.Weather
+{
+    public interface IWeatherService
+    {
+        Task<double[]> GetWeather(string location);
+    }
+}
---
+++ ForgeAir.Core/Services/Weather/WeatherService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Melanchall.DryWetMidi.Interaction;
+using Newtonsoft.Json.Linq;
+using OpenSky.Core.Configuration;
+using OpenSky.Core.Service;
+
+namespace ForgeAir.Core.Services.Weather
+{
+   public class WeatherService : IWeatherService
+   {
+        public WeatherService() {
+            DotNetEnv.Env.TraversePath().Load();
+        } 
+        public async Task<double[]> GetWeather(string location) {
+            var ossConfig = new OssConfiguration { ApiKey = System.Environment.GetEnvironmentVariable("WEATHERAPI_KEY") };
+            var httpClient = new HttpClient();
+            var ossService = new OpenSkyService(httpClient, ossConfig);
+            var jsonResult = await ossService.GetForecastJsonAsync(location);
+            if (jsonResult.IsSuccess)
+            {
+                var searchResults = jsonResult.Response;
+                JObject data = JObject.Parse(searchResults);
+
+                double tempC = data["current"]["temp_c"].Value<double>();
+                double tempF = data["current"]["temp_f"].Value<double>();
+                return new double[] {tempC, tempF };
+            }
+            else
+            {
+                Debug.WriteLine($"Error: {jsonResult.Error!.Message}");
+                return new double[] { double.NaN, double.NaN };
+            }
+        }
+   }
+}
