88d9de30bb4e0d1450f993cc9cdac0e3b26c2039
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2025-11-30 00:40:21 +0200

    added a todo bass helper

---
+++ ForgeAir.Core/Helpers/BassCheatsheet.cs
@@ -0,0 +1,20 @@
+﻿using ManagedBass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ForgeAir.Core.Helpers
+{
+    public static class BassCheatsheet
+    {
+        public static string LastErrorToDetailedError(Errors error)
+        {
+            // waiting for managedbass to release an updated version that contains my changes for description attributes
+            // https://github.com/ManagedBass/ManagedBass/pull/137
+            return GeneralHelpers.GetEnumDescription((Errors)error);
+        }
+    }
+}
