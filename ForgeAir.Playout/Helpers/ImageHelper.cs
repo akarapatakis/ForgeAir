2853639dfa9155ca4f8f552590a24ebe7ece8dca
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2025-11-30 00:37:15 +0200

    added extra helpers

--- ForgeAir.Playout/Helpers/ImageHelper.cs
+++ ForgeAir.Playout/Helpers/ImageHelper.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ForgeAir.Playout.Helpers
 {
@@ -27,5 +28,47 @@ namespace ForgeAir.Playout.Helpers
             }
             return bitmap;
         }
+        public static Bitmap BitmapImageToBitmap(BitmapImage bitmapImage)
+        {
+            if (bitmapImage == null) return null;
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                encoder.Save(outStream);
+                outStream.Seek(0, SeekOrigin.Begin);
+
+                using (var bmp = new Bitmap(outStream))
+                {
+                    return new Bitmap(bmp);
+                }
+            }
+        }
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                return bitmapimage;
+            }
+        }
+        public static BitmapImage LoadBitmapImage(string filePath)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.UriSource = new Uri(filePath);
+            bitmap.EndInit();
+            bitmap.Freeze();
+            return bitmap;
+        }
+
     }
 }
